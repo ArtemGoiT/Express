1bb577df40d0ba86d8145ff06dac58e2a786bb2a
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-07-22 21:22:28 +0200

    fixx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-// src/index.js
+import { startServer } from "./server.js";
 
-import express from "express";
-
-const PORT = 3000;
-
-const app = express();
-
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
