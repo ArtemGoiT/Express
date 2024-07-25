a53993d284fb2db09a78a296a2db6fb0bfd0a786
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-07-25 21:10:46 +0200

    fixxxx

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pino from "pino-http";
 import cors from "cors";
+import { getAllStudents, getStudentById } from "./services/students.js";
 
 import { env } from "./utils/env.js";
 
@@ -19,23 +20,29 @@ export const startServer = () => {
       },
     })
   );
+  app.get("/students", async (req, res) => {
+    const students = await getAllStudents();
 
-  app.get("/", (req, res) => {
-    res.json({
-      message: "Hello World!",
+    res.status(200).json({
+      data: students,
     });
   });
 
-  app.use("*", (req, res, next) => {
-    res.status(404).json({
-      message: "Not found",
-    });
-  });
+  app.get("/students/:studentId", async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: "Something went wrong",
-      error: err.message,
+    // Відповідь, якщо контакт не знайдено
+    if (!student) {
+      res.status(404).json({
+        message: "Student not found",
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      data: student,
     });
   });
 
